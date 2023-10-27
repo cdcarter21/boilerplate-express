6e61d3aa3a1914f167e1f27ecd386c8dc6778c2b
Cole <coltondcarter21@gmail.com>
2023-10-27 13:17:10 -0500

    Added sendFile

--- myApp.js
+++ myApp.js
@@ -4,10 +4,13 @@ let app = express();
 console.log("Hello World");
 
 
-app.get("/", function(req, res) {
-    res.send("Hello Express");
-  });
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+//   });
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 
 
